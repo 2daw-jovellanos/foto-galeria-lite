a8f8979ddeab682c746cad085709b550bb265254
vjfernandez <victor.fernandezgil@educa.madrid.org>
2019-11-27 03:01:17 +0100

    files

--- controladores/GaleriaController.php
+++ controladores/GaleriaController.php
@@ -19,7 +19,7 @@ class GaleriaController extends Controller {
 
     function recibirFichero() {
         $error_msg="";
-        $name = strtolower($name);
+        $name = strtolower($_FILES["foto"]["name"]);
         if (endsWith($name, "jpg") || endsWith($name, "png") || endsWith($name, "gif")
         || endsWith($name, "webp")|| endsWith($name, "jpeg")) {
             // copiar fichero
